ce14755d8e45d74234c36a12691a013518ed75f8
Julian Johannesen <Julian.Johannesen@gmail.com>
2022-05-18 13:59:06 -0400

    next buttons working

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,37 +1,54 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { FaArrowRight } from "react-icons/fa";
 import data from "../../static.json";
-const {bookables} = data;
- 
-export default function BookablesList () {
+
+export default function BookablesList() {
+    
+    const { bookables } = data;
 
     // Hard code the group to "Rooms" for now
-  const group = "Rooms";
- 
-  // Filter for an array of "Rooms"
-  const bookablesInGroup = bookables.filter(bookable => bookable.group === group);
-  
-  const [bookableIndex, setBookableIndex] = useState(1)
+    const group = "Rooms";
 
-  function changeBookable(selectedIndex){
-    setBookableIndex(selectedIndex);
-    console.log("The bookable index is ", selectedIndex);
-  }
- 
-  return (
-    <ul className="bookables items-list-nav">
-      {bookablesInGroup.map((bookable, index) => (
-        <li
-          key={bookable.id} 
-          className={index === bookableIndex ? "selected" : null}
-        >
-          <button
-            className="btn"
-            onClick={()=>changeBookable(index)}
-          >
-            {bookable.title}
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
+    // Filter for an array of "Rooms"
+    const bookablesInGroup = bookables.filter(bookable => bookable.group === group);
+
+    const [bookableIndex, setBookableIndex] = useState(1)
+
+    function nextBookable() {
+        setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
+        console.log("The bookable index is ", bookableIndex);
+    }
+
+    return (
+        <div>
+
+            <ul className="bookables items-list-nav">
+                {bookablesInGroup.map((bookable, index) => (
+                    <li
+                        key={bookable.id}
+                        className={index === bookableIndex ? "selected" : null}
+                    >
+                        <button
+                            className="btn"
+                            onClick={()=>setBookableIndex(index)}
+                        >
+                            {bookable.title}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+
+            <p>
+                <button
+                    autoFocus
+                    className="btn"
+                    onClick={nextBookable}
+                >
+                    <FaArrowRight />
+                    <span>Next</span>
+                </button>
+            </p>
+
+        </div>
+    );
 }
